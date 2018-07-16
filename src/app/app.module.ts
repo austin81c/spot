cd95e052c521c54fc852628d32158face5fbb69a
Austin Clark <austin81clark@msn.com>
2018-07-16 00:42:32 -0600

    Add material layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { MaterialModule } from './material/material.module';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
-    MaterialModule
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
